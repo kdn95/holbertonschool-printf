3401d33081c7e05e11ea5b931776d35935d089a3
Khang <khangdn95@gmail.com>
2024-03-29 04:45:18 +0000

    added stuff

--- _printf.c
+++ _printf.c
@@ -1,22 +1,60 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <stddef.h>
 /**
- *
+ * _printf - simple printf function
+ * @format: format specifiers as char
+ * Return: input
  */
 int _printf(const char *format, ...)
 {
-prf_t prf_s[] = {
-{"%", prf_pct},
-{"c", prf_char},
-{"s", prf_str},
-{"d", prf_int},
-{"i", prf_int},
-{NULL, NULL},
-};
 va_list input_list;
-va_start(input_list, format);
+int i;
+i = 0;
 if (format == NULL)
 {
 return (-1);
+}
+va_start(input_list, format);
+while (*format != '\0')
+{
+if (*format != '%')
+{
+_putchar(*format);
+i++;
+}
+else
+{
+format++;
+if (*format == '\0')
+break;
+if (*format == '%')
+{
+putchar('%');
+i++;
+}
+else if (*format == 'c')
+{
+char c = (char)va_arg(input_list, int);
+_putchar(c);
+i++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(input_list, char *);
+size_t str_len = _strlen(str);
+size_t j = 0;
+while (j < str_len)
+{
+_putchar(str[j]);
+i++;
+j++;
+}
+}
+}
+format++;
+}
+va_end(input_list);
+return (i);
 }
--- main.h
+++ main.h
@@ -1,18 +1,7 @@
 #ifndef _PRINT_F
 #define _PRINT_F
 #include <stdarg.h>
-/**
- * struct prf - struct printf
- * @spf: the fmt specifier
- * @f: function associated
- */
-typedef struct prf
-{
-char *spf;
-int (*f)(va_list);
-} prf_t;
-
-
 int _printf(const char *format, ...);
-
+int _strlen(char *s);
+int _putchar(char ch);
 #endif
