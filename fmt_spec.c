7e1ef4ffbddd17bb0fb6b1e5c81343c47cc9181d
LukeRRT <aumrungruj.t@gmail.com>
2024-04-03 23:58:22 +0000

    print_integer_update

--- fmt_spec.c
+++ fmt_spec.c
@@ -36,3 +36,38 @@ int print_percent(__attribute__((unused))va_list list)
 _writechar('%');
 return (1);
 }
+
+/**
+ * print_integer - Print an integer
+ * @list: list of arguments
+ *
+ * Return: Number of characters printed
+ */
+
+int print_integer(va_list list)
+{
+	int num = va_arg(list, int);
+	int printed = 0;
+	int divisor;
+
+	if (num < 0)
+	{
+		_writechar(-);
+		printed++;
+		num = -num;
+	}
+
+	divisor = 1;
+	while (num / divisor > 9)
+	{
+		divisor *= 10;
+	}
+	while (divisor != 0)
+	{
+		_writechar((num / divisor) + '0');
+		printed++;
+		num %= divisor;
+		divisor /= 10;
+	}
+	return printed;
+}
